eaef58cabafd6279db6419f22eabc86638107fe3
Qinyubaixun <3360233144@qq.com>
2024-04-07 21:55:54 +0800

    Initial Commit

--- k-means.py
+++ k-means.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 import seaborn as sns
 import sqlite3
+from datetime import datetime
+from datetime import date
 from sklearn.cluster import KMeans
 from pylab import mpl
+from matplotlib.dates import AutoDateLocator, DateFormatter
+autodates = AutoDateLocator()
+yearsFmt = DateFormatter('%Y-%m-%d %H:%M:%S')
+
 # 设置显示中文字体
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
 cwd = os.getcwd()  #获得当前的文件路径
@@ -18,7 +24,9 @@ data = cursor.fetchall() #抓取数据
 
 loads_df = pd.DataFrame(data, columns=['id','date','energy_use'])
 loads_df.head()
-
+time = loads_df.date[0:95]
+x_time= [datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in time]  # 将时间字符串转换为时间格式
+print(x_time)
 print("当前数据集含有%s行,%s列"%(loads_df.shape[0],loads_df.shape[1]))
 print(" 最早时间: %s \n 最晚时间: %s"%(loads_df.date.min(),loads_df.date.max()))
 
@@ -93,32 +101,32 @@ class EnergyFingerPrints():
 
     def plot(self):  # 分别绘制各簇中的用户用电行为曲线，并绘制各簇的平均用电行为曲线
         self.cluster_names = [str(x) for x in range(self.n_clusters)]
-        fig, ax = plt.subplots(figsize=(18, 16))
-
+        fig, ax = plt.subplots(2, 2, figsize=(12, 8))
         for i in range(0, self.n_clusters):
-            all_data = []
+            # all_data = []
+            plt.subplot(2, 2, i + 1)
             for x, y in zip(self.data, self.predictions):
                 if y == i:
-                    all_data.append(x)
-                    plt.subplot(4, 1, i + 1)
+                    # all_data.append(x)
                     plt.plot(x, alpha=0.06, color="blue")
-                    # plt.ylim(0,4)
+                    plt.ylim(0, 20)
                     plt.xlim(0, 96)
                     plt.title('Cluster {}'.format(i + 1))
                     plt.ylabel('用电量/kW')
 
-            all_data_array = np.array(all_data)
-            mean = all_data_array.mean(axis=0)
-            self.means.append(mean)
-            plt.plot(mean, color="black", linewidth=4)
-
+            # all_data_array = np.array(all_data)
+            # mean = all_data_array.mean(axis=0)
+            # self.means.append(mean)
+            plt.plot(self.kmeans.cluster_centers_[i], color="black", linewidth=4)
+            plt.xlim(0, 96)
+            plt.ylim(0, 20)
         plt.show()
 
     def plot_energy_fingerprints(self):  # 将各簇的用电行为数据绘制在一张表上
         fig, ax = plt.subplots(figsize=(8, 5))
 
-        for i, item in enumerate(self.means):
-            plt.plot(item, label="cluster %s" % (str(i + 1)))
+        for i in range(0, self.n_clusters):
+            plt.plot(self.kmeans.cluster_centers_[i], label="cluster %s" % (str(i + 1)))
             plt.xlim(0, 96)
         plt.ylabel('用电量/kW')
         plt.legend()
